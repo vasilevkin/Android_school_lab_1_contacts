04692b0147651a86c8802073766af5fcab049c00
Sergey Vasilevkin <svasilevkin@gmail.com>
2020-05-02 16:24:06 +0300

    Add Contact Details fragment with a viewModel.

---
+++ app/src/main/java/com/vasilevkin/greatcontacts/features/contactdetails/view/ContactDetailsFragment.kt
@@ -0,0 +1,35 @@
+package com.vasilevkin.greatcontacts.features.contactdetails.view
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.vasilevkin.greatcontacts.R
+import com.vasilevkin.greatcontacts.features.contactdetails.viewmodel.ContactDetailsViewModel
+
+class ContactDetailsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() =
+            ContactDetailsFragment()
+    }
+
+    private lateinit var viewModel: ContactDetailsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.contact_details_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(ContactDetailsViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/vasilevkin/greatcontacts/features/contactdetails/viewmodel/ContactDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.vasilevkin.greatcontacts.features.contactdetails.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class ContactDetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
