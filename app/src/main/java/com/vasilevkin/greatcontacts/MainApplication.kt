ac98861c10a0f3a3fff926c0f035d04286de1f5c
Sergey Vasilevkin <svasilevkin@gmail.com>
2020-05-03 11:42:53 +0300

    Add Stetho.

--- app/src/main/java/com/vasilevkin/greatcontacts/MainApplication.kt
+++ app/src/main/java/com/vasilevkin/greatcontacts/MainApplication.kt
@@ -1,6 +1,7 @@
 package com.vasilevkin.greatcontacts
 
 import android.app.Application
+import com.facebook.stetho.Stetho
 import com.vasilevkin.greatcontacts.di.AppComponent
 import com.vasilevkin.greatcontacts.di.DaggerAppComponent
 
@@ -15,4 +16,27 @@ class MainApplication : Application() {
         return DaggerAppComponent.factory().create(applicationContext)
     }
 
+    override fun onCreate() {
+        super.onCreate()
+
+        // Create an InitializerBuilder
+        val initializerBuilder = Stetho.newInitializerBuilder(this).also {
+
+            // Enable Chrome DevTools
+            it.enableWebKitInspector(
+                Stetho.defaultInspectorModulesProvider(this)
+            )
+
+            // Enable command line interface
+            it.enableDumpapp(
+                Stetho.defaultDumperPluginsProvider(this)
+            )
+        }
+
+        // Use the InitializerBuilder to generate an Initializer
+        val initializer = initializerBuilder.build()
+
+        // Initialize Stetho with the Initializer
+        Stetho.initialize(initializer)
+    }
 }
