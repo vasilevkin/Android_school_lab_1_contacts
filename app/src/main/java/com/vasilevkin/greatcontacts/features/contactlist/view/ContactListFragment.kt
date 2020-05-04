ab7b2ecaac541386298c7609b7a3755bb295592b
Sergey Vasilevkin <svasilevkin@gmail.com>
2020-05-04 18:18:01 +0300

    Implement search in contact list.

--- app/src/main/java/com/vasilevkin/greatcontacts/features/contactlist/view/ContactListFragment.kt
+++ app/src/main/java/com/vasilevkin/greatcontacts/features/contactlist/view/ContactListFragment.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -19,10 +22,13 @@ import com.vasilevkin.greatcontacts.models.Person
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.internal.schedulers.ComputationScheduler
+import kotlinx.android.synthetic.main.contact_list_fragment.*
+import java.util.*
 import javax.inject.Inject
+import kotlin.collections.ArrayList
 
 
-class ContactListFragment : Fragment() {
+class ContactListFragment : Fragment(), Filterable {
 
     companion object {
         fun newInstance() =
@@ -33,6 +39,7 @@ class ContactListFragment : Fragment() {
     lateinit var viewModel: ContactListViewModel
 
     private var contacts: List<IComparableItem> = emptyList()
+    private var contactsFilteredList: List<IComparableItem> = emptyList()
     private lateinit var diffAdapter: DiffUtilCompositeAdapter
     private var contactsList: RecyclerView? = null
     private var disposable: Disposable? = null
@@ -86,6 +93,17 @@ class ContactListFragment : Fragment() {
         (activity as AppCompatActivity).supportActionBar?.title = "Contacts"
 
         viewModel.view = activity as Context
+
+        contact_search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                filter.filter(newText)
+                return false
+            }
+        })
     }
 
     override fun onStart() {
@@ -114,6 +132,7 @@ class ContactListFragment : Fragment() {
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeOn(ComputationScheduler())
             .subscribe {
+                this.contacts = it
                 displayContacts(it)
             }
     }
@@ -123,12 +142,45 @@ class ContactListFragment : Fragment() {
         this.disposable?.dispose()
     }
 
+    // interface Filterable
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val charSearch = constraint.toString()
+                contactsFilteredList = if (charSearch.isEmpty()) {
+                    contacts
+                } else {
+                    val resultList = ArrayList<IComparableItem>()
+                    for (row in contacts) {
+                        if (row.content().toString().toLowerCase(Locale.ROOT).contains(
+                                charSearch.toLowerCase(
+                                    Locale.ROOT
+                                )
+                            )
+                        ) {
+                            resultList.add(row)
+                        }
+                    }
+                    resultList
+                }
+                val filterResults = FilterResults()
+                filterResults.values = contactsFilteredList
+                return filterResults
+            }
+
+            @Suppress("UNCHECKED_CAST")
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                contactsFilteredList = results?.values as ArrayList<IComparableItem>
+                displayContacts(contactsFilteredList)
+            }
+        }
+    }
+
     // Private methods
 
     private fun displayContacts(list: List<IComparableItem>) {
-        this.contacts = list
-
-        diffAdapter.swapData(contacts)
+        diffAdapter.swapData(list)
         contactsList?.scrollToPosition(0)
     }
 }
