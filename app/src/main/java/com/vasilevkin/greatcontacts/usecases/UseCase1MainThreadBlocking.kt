aae5fdab077713a70aece617b0eb169d093c10d3
Sergey Vasilevkin <svasilevkin@gmail.com>
2020-05-07 13:45:48 +0300

    Implement UseCase1MainThreadBlocking.

---
+++ app/src/main/java/com/vasilevkin/greatcontacts/usecases/UseCase1MainThreadBlocking.kt
@@ -0,0 +1,29 @@
+package com.vasilevkin.greatcontacts.usecases
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.vasilevkin.greatcontacts.models.Person
+import com.vasilevkin.greatcontacts.repository.datasource.ILocalDataSource
+
+
+class UseCase1MainThreadBlocking(
+    private val localDataSource: ILocalDataSource
+) : IUseCase {
+
+    private val mutableLiveData = MutableLiveData<List<Person>>()
+
+    override var context: Context? = null
+
+    // interface IUseCase
+
+    override fun getPersons(): LiveData<List<Person>> {
+        localDataSource.context = context
+
+        val list = localDataSource.getContacts()
+
+        mutableLiveData.postValue(list)
+
+        return mutableLiveData
+    }
+}
